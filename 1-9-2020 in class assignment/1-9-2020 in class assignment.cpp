0eefa2fe37d63976263af16f96b88e7f730b91c0
boile <boile@DESKTOP-ALSOR61>
2020-01-09 10:33:47 -0700

    class ended, but was trying to figure out how to write the equasion for the area of a hexagon

--- 1-9-2020 in class assignment/1-9-2020 in class assignment.cpp
+++ 1-9-2020 in class assignment/1-9-2020 in class assignment.cpp
@@ -4,12 +4,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    float kilogram, pound;
+    /*float kilogram, pound;
     cout << "Enter pound(s): ";
     cin >> pound;
     kilogram = pound * .454;
     cout << pound << " pound = " << kilogram << " kilogram(s): ";
 
     cout << "end of part 1";
-	return 0;
+	return 0; */
+    
+    float area, length;
+    cout << "Enter side length of hexagon: ";
+    cin >> length;
+    area = 3(sqrt(3)) / 2 * (length*length);
+
+
+   
+    
 }
